9a618a9d058ce81ad2c38dc8f80d816206b9536a
KHI(home) <alfkcjstk853@naver.com>
2023-09-30 09:42:15 +0900

    construct class Player

--- track_random_hand.py
+++ track_random_hand.py
@@ -1,12 +1,15 @@
 from pico2d import *
 open_canvas()  
-sprite = load_image('animation_sheet.png')
+
 backGround = load_image('TUK_GROUND_FULL.png')
 hand = load_image('hand_arrow.png')
 
+class Player:
+    def __init__(self):
+        print("class Player construct")
 clear_canvas()
 backGround.draw(400,400)
-sprite.draw(400,400)
+player = Player()
 hand.draw(400,400)
 update_canvas()
 delay(1)
