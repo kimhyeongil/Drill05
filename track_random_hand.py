e465a6274622403cbaf61152b706f06625462fd6
KHI(home) <alfkcjstk853@naver.com>
2023-09-30 09:43:39 +0900

    draw player as member func

--- track_random_hand.py
+++ track_random_hand.py
@@ -6,10 +6,15 @@ hand = load_image('hand_arrow.png')
 
 class Player:
     def __init__(self):
-        print("class Player construct")
+        self.sprite = load_image('animation_sheet.png')
+        self.pos = [get_canvas_width() // 2, get_canvas_height() // 2]
+    def draw(self):
+        x, y = 0, 1
+        self.sprite.draw(self.pos[x], self.pos[y])
 clear_canvas()
 backGround.draw(400,400)
 player = Player()
+player.draw()
 hand.draw(400,400)
 update_canvas()
 delay(1)
