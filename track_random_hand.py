0b290457d5e0213a437250628b8386c2501ee3f3
KHI(home) <alfkcjstk853@naver.com>
2023-09-30 09:51:45 +0900

    make running anima

--- track_random_hand.py
+++ track_random_hand.py
@@ -8,15 +8,20 @@ class Player:
     def __init__(self):
         self.sprite = load_image('animation_sheet.png')
         self.pos = [get_canvas_width() // 2, get_canvas_height() // 2]
+        self.nFrame, self.frame = 8, 0
     def draw(self):
         x, y = 0, 1
-        self.sprite.clip_draw(0, 0, 100, 100, self.pos[x], self.pos[y])
-clear_canvas()
-backGround.draw(400,400)
+        self.sprite.clip_draw(self.frame * 100, 0, 100, 100, self.pos[x], self.pos[y])
+        self.frame = (self.frame + 1) % self.nFrame
 player = Player()
-player.draw()
-hand.draw(400,400)
-update_canvas()
+while True:
+    clear_canvas()
+    backGround.draw(400,400)
+    player.draw()
+    hand.draw(400,400)
+    update_canvas()
+    delay(0.1)
+
 delay(1)
 
 close_canvas()
