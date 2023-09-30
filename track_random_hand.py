253b1a65e5e43488939a7603853bd837b0da3005
KHI(home) <alfkcjstk853@naver.com>
2023-09-30 10:38:55 +0900

    Drill 05 제출 (2020182013 김형일)

--- track_random_hand.py
+++ track_random_hand.py
@@ -6,14 +6,21 @@ class Player:
         self.sprite = load_image('animation_sheet.png')
         self.pos = [get_canvas_width() // 2, get_canvas_height() // 2]
         self.nFrame, self.frame = 8, 0
-    
+        self.dir = 'right'
     def draw(self):
         x, y = 0, 1
-        self.sprite.clip_draw(self.frame * 100, 0, 100, 100, self.pos[x], self.pos[y])
+        if(self.dir == 'left'):
+            self.sprite.clip_draw(self.frame * 100, 0, 100, 100, self.pos[x], self.pos[y])
+        elif(self.dir == 'right'):
+            self.sprite.clip_composite_draw(self.frame * 100, 0, 100, 100, 0, 'h', self.pos[x], self.pos[y], 100, 100)
         self.frame = (self.frame + 1) % self.nFrame
 
     def track(self, target, t):
         x, y = 0,1
+        if(target[x] > self.pos[x]):
+            self.dir = 'right'
+        else :
+            self.dir = 'left'
         self.pos[x] = (1 - t) * self.pos[x] + t * target[x]
         self.pos[y] = (1 - t) * self.pos[y] + t * target[y]
 
@@ -30,8 +37,6 @@ class Hand:
         self.img.draw(self.pos[x], self.pos[y])
 
 
-def isCollide(p1, p2, gap):
-    return abs(p1[0] - p2[0]) < gap and abs(p1[1] -p2[1]) < gap
 class GameManager:
     def __init__(self):
         open_canvas(1024,720)
@@ -49,9 +54,12 @@ class GameManager:
         update_canvas()
         delay(0.1)
 
+    def isCollide(self, p1, p2, gap):
+        return abs(p1[0] - p2[0]) < gap and abs(p1[1] -p2[1]) < gap
+    
     def logic(self):
         self.player.track(self.hand.pos, 0.2)
-        if( isCollide(self.hand.pos, self.player.pos, self.hand.img.w // 2)):
+        if( self.isCollide(self.hand.pos, self.player.pos, self.hand.img.w // 2)):
             self.hand.set_random_pos()
 
 GM = GameManager()
